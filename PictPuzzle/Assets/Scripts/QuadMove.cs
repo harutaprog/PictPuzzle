e23bf033353901bf5854cf8fa41f517d8100a819
HarutaProg <haruta.prog1999@gmail.com>
2019-06-03 18:11:02 +0900

    足場を作成するスクリプトを一部制作

--- PictPuzzle/Assets/Scripts/QuadMove.cs
+++ PictPuzzle/Assets/Scripts/QuadMove.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class QuadMove : MonoBehaviour
 {
+
+    // X, Y座標の移動可能範囲
+    [System.Serializable]
+    public class Bounds
+    {
+        public float xMin, xMax, yMin, yMax;
+    }
+    [SerializeField] Bounds bounds;
+
     Vector3 mousepos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +24,10 @@ public class QuadMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        gameObject.transform =
+        mousepos = Input.mousePosition;
+        gameObject.transform.position = new Vector3(
+           Mathf.Clamp(Mathf.Round(Camera.main.ScreenToWorldPoint(mousepos).x),bounds.xMin,bounds.xMax),
+           Mathf.Clamp(Mathf.Round(Camera.main.ScreenToWorldPoint(mousepos).y),bounds.yMin,bounds.yMax),
+           0);
     }
 }
