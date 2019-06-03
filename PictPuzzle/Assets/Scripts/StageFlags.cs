a38e870abf6b6fc9b57e10afa57799566e168477
HarutaProg <haruta.prog1999@gmail.com>
2019-06-03 15:59:09 +0900

    箱を作成する機能を製作開始

--- PictPuzzle/Assets/Scripts/StageFlags.cs
+++ PictPuzzle/Assets/Scripts/StageFlags.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using UnityEngine.SceneManagement;
@@ -14,7 +13,7 @@ public class StageFlags : SingletonMonoBehaviour<StageFlags>
         false, false, false, false, false };
 
     private AsyncOperation async;
-    private GameObject loadUI;
+    private Canvas canvas;
 
     public void Awake()
     {
@@ -25,10 +24,8 @@ public class StageFlags : SingletonMonoBehaviour<StageFlags>
         }
 
         DontDestroyOnLoad(gameObject);
-        loadUI = GameObject.FindGameObjectWithTag("LoadUI");
-        loadUI.GetComponent<Canvas>().enabled = false;
-        Debug.Log(loadUI);
-        Debug.Log("awake");
+        canvas = GameObject.FindGameObjectWithTag("LoadUI").GetComponent<Canvas>();
+        canvas.GetComponent<Canvas>().enabled = false;
         FileLoad();
     }
 
@@ -49,29 +46,21 @@ public class StageFlags : SingletonMonoBehaviour<StageFlags>
         Debug.Log(savejson);
         File.WriteAllText("Assets\\FlagDatas.json", savejson);
         Debug.Log("File Save");
-
     }
 
     public void FlagTrue(int i)
     {
-        if(instance.Flags[i]!=true) instance.Flags[i] = true;
+        if (instance.Flags[i] != true) instance.Flags[i] = true;
     }
 
     IEnumerator Load(string sceneName)
     {
-        Debug.Log("ColPlay");
         async = SceneManager.LoadSceneAsync(sceneName);
-        loadUI.GetComponent<Canvas>().enabled = true;
+        canvas.GetComponent<Canvas>().enabled = true;
         while (!async.isDone)
         {
-            //            var proressVal = Mathf.Clamp01(async.progress / 0.9f);
-            //            slider.value = proressVal;
-            Debug.Log("loading");
             yield return null;
         }
-        //      loadUI.GetComponent<Image>().enabled = false;
-        Debug.Log("active");
-        loadUI.GetComponent<Canvas>().enabled = false;
+        canvas.GetComponent<Canvas>().enabled = false;
     }
-
 }
