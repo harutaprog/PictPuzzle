0d9e7fb52d1b576f3b3245c306751b32a26e00ba
Tatsu-KB <kubo2019cooljapan@gmail.com>
2019-06-24 15:32:17 +0900

    移動修正

--- PictPuzzle/Assets/Move_Scripts/Hit_Under.cs
+++ PictPuzzle/Assets/Move_Scripts/Hit_Under.cs
@@ -6,10 +6,12 @@ public class Hit_Under : MonoBehaviour
 {
     //下部分の判定
     public Move_Player player;
+    bool Under;
     // Start is called before the first frame update
     void Start()
     {
         player =  transform.parent.GetComponent<Move_Player>();
+        Under = true;
     }
 
     // Update is called once per frame
@@ -20,7 +22,12 @@ public class Hit_Under : MonoBehaviour
 
     public void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.gameObject.tag != "Cursor") 
+        if(collision.gameObject.tag != "Cursor" && Under) 
         player.Jump();
+        Under = false;
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        Under = true;
     }
 }
