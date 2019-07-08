2ee4bb1c350d7b21f3410a928c2fd6a6888ced44
HarutaProg <haruta.prog1999@gmail.com>
2019-07-08 15:18:10 +0900

    ステージクリア時のフラグ建て＆ステージセレクトその他を修正＆追加

--- PictPuzzle/Assets/Scripts/Pouse.cs
+++ PictPuzzle/Assets/Scripts/Pouse.cs
@@ -9,16 +9,24 @@ public class Pouse : MonoBehaviour
     [SerializeField]
     GameObject backImage;
     [SerializeField]
+    GameObject PauseButtons;
+    [SerializeField]
+    GameObject ClearButtons;
+    [SerializeField]
     Text text;
     [SerializeField]
     CursorController cursorController;
     [SerializeField]
     Move_Player player;
+    [SerializeField]
+    private int StageID;
 
     // Start is called before the first frame update
     void Awake()
     {
         backImage.SetActive(false);
+        PauseButtons.SetActive(false);
+        ClearButtons.SetActive(false);
     }
 
     // Update is called once per frame
@@ -31,6 +39,7 @@ public class Pouse : MonoBehaviour
                 Time.timeScale = 0;
                 cursorController.CursorFalse();
                 backImage.SetActive(true);
+                PauseButtons.SetActive(true);
                 text.text = ("ポーズ中");
             }
 
@@ -39,15 +48,27 @@ public class Pouse : MonoBehaviour
                 Time.timeScale = 1;
                 cursorController.CursorTrue();
                 backImage.SetActive(false);
-
+                PauseButtons.SetActive(false);
             }
         }
-        if(player.Death == true)
+        if (player.Death == true)
         {
-            if(Time.timeScale != 0)Time.timeScale = 0;
+            if (Time.timeScale != 0) Time.timeScale = 0;
             cursorController.CursorFalse();
             backImage.SetActive(true);
+            PauseButtons.SetActive(true);
             text.text = ("ゲームオーバー");
+        }
+
+        if (player.Clear == true)
+        {
+            if (Time.timeScale != 0) Time.timeScale = 0;
+            cursorController.CursorFalse();
+            StageFlags.instance.FlagTrue(StageID);
+            StageFlags.instance.FileSave();
+            backImage.SetActive(true);
+            ClearButtons.SetActive(true);
+            text.text = ("ステージクリア");
         }
     }
 }
