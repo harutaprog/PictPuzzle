0aac680746ae8fb84152121f2dbf6762db305b69
HarutaProg <haruta.prog1999@gmail.com>
2019-07-22 17:01:40 +0900

    no message

---
+++ PictPuzzle/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtons : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> StageSelectButtons;
+    private int ListNum;
+
+
+    public void Scroll_Left()
+    {
+        if (ListNum < StageSelectButtons.Count)
+        {
+            ListNum++;
+            for (int i = 0; i < StageSelectButtons.Count; i++)
+            {
+                if (i == ListNum) StageSelectButtons[i].SetActive(true);
+                else StageSelectButtons[i].SetActive(false);
+            }
+        }
+    }
+
+    public void Scroll_Riget()
+    {
+        if (ListNum < StageSelectButtons.Count)
+        {
+            ListNum--;
+            for (int i = 0; i < StageSelectButtons.Count; i++)
+            {
+                if (i == ListNum) StageSelectButtons[i].SetActive(true);
+                else StageSelectButtons[i].SetActive(false);
+            }
+        }
+    }
+}
