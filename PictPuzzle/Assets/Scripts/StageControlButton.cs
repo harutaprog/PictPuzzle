1791d131ad46025d19a7e867903cd407c54a5d48
HarutaProg <haruta.prog1999@gmail.com>
2019-07-22 15:12:35 +0900

    画像系の整理整頓

--- PictPuzzle/Assets/Scripts/StageControlButton.cs
+++ PictPuzzle/Assets/Scripts/StageControlButton.cs
@@ -6,29 +6,35 @@ using UnityEngine.SceneManagement;
 
 public class StageControlButton : MonoBehaviour
 {
-    //移動したいシーンの名前(何もない場合"void"シーン)
+    //移動したいシーンの名前(特に指定しなかった場合"void"シーンに移るヨ)
     [SerializeField]
     private string SceneName = "void";
 
-    public void StageLoad()
+
+    //SceneNameで指定したシーンに移動するスクリプト(大文字小文字も区別するヨ。間違えるとエラーだから気を付けて)
+    public void SceneLoad()
     {
         StageFlags.instance.StartCoroutine("Load", SceneName);
     }
 
+    //ゲームを終了させるスクリプト
     public void GameExit()
     {
         UnityEditor.EditorApplication.isPlaying = false;
         UnityEngine.Application.Quit();
     }
 
+    //ステージセレクト画面に移動するスクリプト(名前指定はいらないヨ)
     public void SelectBack()
     {
         StageFlags.instance.StartCoroutine("Load", "StageSelect");
     }
 
+    //同じシーンを再読み込みさせるスクリプト
     public void Reroad()
     {
         Scene loadScene = SceneManager.GetActiveScene();
         StageFlags.instance.StartCoroutine("Load", loadScene.name);
     }
+
 }
