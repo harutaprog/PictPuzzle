09c8e9571bb7c52cd4b5532ed79b8aa87119c76a
HarutaProg <haruta.prog1999@gmail.com>
2019-09-02 18:00:11 +0900

    抜けていた背景を追加&スクリプトの調整

--- PictPuzzle/Assets/Scripts/StageControlButton.cs
+++ PictPuzzle/Assets/Scripts/StageControlButton.cs
@@ -25,7 +25,7 @@ public class StageControlButton : MonoBehaviour
     public void GameExit()
     {
         audioSource.Play();
-        UnityEditor.EditorApplication.isPlaying = false;
+//        UnityEditor.EditorApplication.isPlaying = false;
         UnityEngine.Application.Quit();
     }
 
